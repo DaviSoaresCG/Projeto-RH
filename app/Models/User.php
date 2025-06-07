b783c3ba9e8cbdc4650e0c2a1563db60d5565c41
davi-scg <davi.gomes2@estudante.ifto.edu.br>
2025-06-07 16:43:55 -0300

    relacionamentos

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    use HasFactory;
+    
+    // relacionamento de 1 para 1 com a tabela User_detail
+    public function detail()
+    {
+        return $this->hasOne(UserDetail::class);
+    }
+
+    // belong significa que o usuario pertence a muitos departmentos, 1:n
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/email', function(){
+    Mail::raw('Mensagem de teste', function (Message $message){
+        $message->to('davi@gmail.com')
+        ->subject('Bem vindo')
+        ->from('rh@gmail.com');
+    });
+
+    echo "Email enviado";
 });
